c7e672397a17b26536de187c06c29f13f18afa41
RachelWu <119239275@qq.com>
2021-09-12 22:07:31 +0800

    Create Images.go

---
+++ Images.go
@@ -0,0 +1,29 @@
+package main
+
+import "golang.org/x/tour/pic"
+import "image/color"
+import "image"
+
+type Image struct{
+	width int
+	height int
+
+}
+
+func (i Image) ColorModel() color.Model{
+	
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0 , i.width, i.height)
+    
+}
+func (i Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x+y), uint8(x+y), 255, 255}
+}
+
+func main() {
+	m := Image{100, 20}
+	pic.ShowImage(m)
+}
